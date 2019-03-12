0d8dab4bc30b5923f32eb6139eb0f8f788762d4c
Taco <akataco@outlook.com>
2019-03-12 11:54:26 +0800

    basic order card

--- lib/module/order/order_card.dart
+++ lib/module/order/order_card.dart
@@ -10,6 +10,14 @@ class OrderCard extends StatefulWidget {
 
 class OrderCardState extends State<OrderCard> {
 
+  List<Widget> _showDessert() {
+    List<Widget> desserts = new List();
+    for (int i = 0; i < 3; ++i) {
+      desserts.add(_buildDessertItem());
+    }
+    return desserts;
+  }
+
   Widget _buildClockIcon() {
     return Container(
       decoration: BoxDecoration(
@@ -23,17 +31,179 @@ class OrderCardState extends State<OrderCard> {
     );
   }
 
-  Widget _buildOrderCard() {
-    return Flexible(
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.teal.shade700,
-          border: Border.all(color: Colors.black)
+  Widget _buildDessertItem() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.only(top: 10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('爆浆海盐奶盖'),
+                Row(children: <Widget>[
+                  Text(
+                    '巧克力',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 12
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 10.0),
+                    child: Text(
+                      '12寸',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 12
+                      )
+                    ),
+                  )
+                ])
+              ],
+            ),
+          ),
         ),
-        margin: const EdgeInsets.only(top: 10.0),
-        alignment: Alignment.center,
-        child: Text('Hello World', style: Theme.of(context).textTheme.display1.copyWith(color: Colors.white)),
+        Expanded(
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Text('￥128'),
+                Text(
+                  'x1',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 12
+                  ),
+                ),
+              ],
+            ),
+          )
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDessertList() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.only(bottom: 10.0, left: 10.0, right: 10.0),
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(color: Colors.grey)
+              ),
+            ),
+            child: Column(
+              children: _showDessert(),
+            )
+          )
+        )
+      ],
+    );
+  }
+
+  Widget _buildAddress() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(10.0),
+            child: Row(
+              children: <Widget>[
+                Icon(CupertinoIcons.location),
+                Container(
+                  margin: EdgeInsets.only(left: 10.0),
+                  child: Text('大悦城3楼5号车库'),
+                )
+              ],
+            ),
+          )
+        )
+      ],
+    );
+  }
+
+  Widget _buildReceiver() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(10.0),
+            child: Row(
+              children: <Widget>[
+                Icon(CupertinoIcons.phone),
+                Container(
+                  margin: EdgeInsets.only(left: 10.0),
+                  child: Text('1777777777'),
+                ),
+              ],
+            ),
+          )
+        ),
+        Expanded(
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(right: 10.0),
+                  child: Text('张先生'),
+                )
+              ]
+            )
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildRemark() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(10.0),
+            child: Row(
+              children: <Widget>[
+                Icon(CupertinoIcons.tag),
+                Container(
+                  margin: EdgeInsets.only(left: 10.0),
+                  child: Text('不要糖'),
+                )
+              ],
+            ),
+          )
+        )
+      ],
+    );
+  }
+
+  Widget _buildOrderCard() {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
       ),
+      margin: const EdgeInsets.only(top: 10.0),
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          _buildDessertList(),
+          _buildRemark(),
+          _buildReceiver(),
+          _buildAddress(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTime() {
+    return Text(
+      '17:30',
+      textAlign: TextAlign.left,
     );
   }
 
@@ -56,10 +226,7 @@ class OrderCardState extends State<OrderCard> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisSize: MainAxisSize.min,
                   children: <Widget>[
-                    Text(
-                      '17:30',
-                      textAlign: TextAlign.left,
-                    ),
+                    _buildTime(),
                     _buildOrderCard(),
                   ],
                 ),
